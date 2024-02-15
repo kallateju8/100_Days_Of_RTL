f336606223de49845cc46e96330749052fd0f979
kallateju8 <84400547+kallateju8@users.noreply.github.com>
2024-02-15 19:33:34 +0530

    Create environment.sv

---
+++ TB to verify half subtractor [Day 75]/environment.sv
@@ -0,0 +1,24 @@
+`include "transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+
+class environment;
+  generator gen;
+  driver drv;
+  mailbox mbx;
+  virtual hs_if vif;
+  
+  function new(virtual hs_if vif);
+    this.vif=vif;
+    mbx=new();
+    gen=new(mbx);
+    drv=new(vif,mbx);
+  endfunction
+  
+  task run();
+    fork
+      gen.run();
+      drv.run();
+    join
+  endtask
+endclass
