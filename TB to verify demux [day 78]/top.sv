bd5876a29cc7c358ecfd93867bb8f268e7c415ef
kallateju8 <84400547+kallateju8@users.noreply.github.com>
2024-02-19 23:03:37 +0530

    Create top.sv

---
+++ TB to verify demux [day 78]/top.sv
@@ -0,0 +1,15 @@
+`include "interface.sv"
+`include "test.sv"
+
+module top;
+  
+  demux_if pif();
+  test tb(pif);
+  
+  demux m1(.d(pif.d),.sel(pif.sel),.y(pif.y));
+  initial
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars(1);
+    end
+endmodule
